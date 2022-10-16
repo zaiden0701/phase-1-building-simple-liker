1fed4e7a6a69cd43149c025c27f1d488d97ac713
Aiden Zafar <+>
2022-10-16 17:54:35 -0400

    commit tree 40274a99d40a24c8db80c6c198bfce1eee6060b6

--- main.js
+++ main.js
@@ -3,9 +3,46 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {activeHeart()})
 
+const modal = document.querySelector('#modal')
+console.log(modal)
+modal.classList.add('hidden')
 
+ const heart = document.querySelectorAll('span.like-glyph')
+ heart.forEach(activeHeart)
 
+  function activeHeart(hearts){
+    hearts.addEventListener('click', (e) => {
+      mimicServerCall()
+      .then((response) => {
+        if (e.target.innerText === EMPTY_HEART) {
+          e.target.classList.add('activated-heart')
+          e.target.innerText === FULL_HEART
+        }
+        else {
+          e.target.classList.remove('activated-heart')
+          e.target.innerText === EMPTY_HEART
+        }
+      })
+      .catch(function(){
+        setTimeout(() => {modal, 3000})
+      })
+    })
+  }
+
+//   .then(() => {
+//   if (heart.target.innerHTML === EMPTY_HEART) {
+//     heart.target.classList.add('activated heart')
+//     heart.target.innerHTML === FULL_HEART
+//   }
+//   else {
+//     heart.target.classList.remove('activated heart')
+//     heart.target.innerHTML === EMPTY_HEART
+//   }
+// })
+
+// document.getElementsById('').hidden
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
